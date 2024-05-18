417f97052ddd49114de6dc3a5410a50f38e45e5a
vitoscape <vit2001g@gmail.com>
2024-05-18 21:11:36 +0300

    First commit. Program test.

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.CannotWriteException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.TagException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String args[]) throws CannotReadException, TagException, InvalidAudioFrameException, ReadOnlyFileException, IOException, CannotWriteException {
+		
+		File testFile = new File("C:\\Users\\vit20\\Documents\\test\\The Offspring - Nothingtown.flac");	// Загрузка файла
+		
+		AudioFile f = AudioFileIO.read(testFile);		// Чтение аудиофайла
+		Tag tag = f.getTag();							// Получение тегов
+		
+		System.out.printf("%s\n", tag.getFirst(FieldKey.GENRE));
+		
+		tag.setField(FieldKey.GENRE, "Punk-rock");	// Изменение тега
+		f.commit();										// Подтверждение изменения
+		
+		System.out.printf("%s\n", tag.getFirst(FieldKey.GENRE));
+	}
+}
