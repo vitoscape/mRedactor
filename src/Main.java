48579dd0d30fda3992de2e7838fb6a5e2cbffede
vitoscape <vit2001g@gmail.com>
2024-06-30 11:22:25 +0300

    Done with rename files in mode 0

--- src/Main.java
+++ src/Main.java
@@ -97,6 +97,8 @@ public class Main {
 				if (!artist.equals("0")) {
 					tag.setField(FieldKey.ARTIST, artist);
 					tag.setField(FieldKey.ALBUM_ARTIST, artist);
+				} else {
+					artist = tag.getFirst(FieldKey.ARTIST);			// If changing tag value doesn't need then read tag value from file to rename file
 				}
 				if (!album.equals("0")) {
 					tag.setField(FieldKey.ALBUM, album);
@@ -114,10 +116,13 @@ public class Main {
 				
 				audioFile.commit();	// Apply change
 				
+				
 				// Rename file
-				Path source = Paths.get(file.getPath());
-				// TODO: Add extension from source to newName
-				String newName = artist + " - " + tag.getFirst(FieldKey.TITLE);
+				String fileName = file.getName();
+				int dotIndex = fileName.lastIndexOf('.');								// Get index of last  '.' char to get extension
+				String extension = (dotIndex == -1) ? "" : fileName.substring(dotIndex);	// Get extension
+				Path dirPath = Paths.get(file.getPath()).getParent();
+				String newName = artist + " - " + tag.getFirst(FieldKey.TITLE) + extension;	// Create name of file
 				
 				// If new name contains forbidden characters for files then delete these characters
 				if (newName.matches(".*[<>\"/\\\\|?*:].*")) {
@@ -132,7 +137,11 @@ public class Main {
 					newName = newName.replace(":", "");
 				}
 				
-				System.out.printf("%s\t->\t%s\n", source, newName);
+				String newPathName = dirPath + "\\" + newName;								// And finally create new full path name
+				
+				if (!file.renameTo(new File(newPathName))) {
+					System.out.printf("Error when renaming file: %s\n", fileName);
+				}
 			}
 		}
 		
