184d13e284e7c605bdb60b6f161f7410c233de4a
vitaliy <vit2001g@gmail.com>
2024-08-08 13:57:57 +0300

    Create terminal and audio utils

---
+++ src/utils/TerminalUtil.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class TerminalUtil {
+	
+	public static void clearTerminal() {
+		try {
+			final String os = System.getProperty("os.name");
+			
+			if (os.contains("Windows")) {
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();	// Clear terminal in Windows
+			} else {
+				System.out.print("\033[H\033[2J");	// ESC-sequence to clear terminal window
+				System.out.flush();
+			}
+		} catch (final Exception e) {
+			//  Handle any exceptions
+		}
+	}
+}
