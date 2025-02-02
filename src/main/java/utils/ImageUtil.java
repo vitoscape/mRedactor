cc5cad36e4cf8ea719652d024dbb6f5f7064a6b5
vitoscape <vit2001g@gmail.com>
2025-02-02 14:11:13 +0300

    Add ImageUtil class

---
+++ src/main/java/utils/ImageUtil.java
@@ -0,0 +1,63 @@
+package utils;
+
+
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.TagException;
+import org.jaudiotagger.tag.images.Artwork;
+import org.jaudiotagger.tag.images.ArtworkFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class ImageUtil {
+	
+	private static final List<String> IMAGE_EXTENSIONS =
+			Arrays.asList("jpg", "jpeg", "png");
+	
+	/**
+	 * Check if the {@code file} is image.
+	 *
+	 * @param file	file to check
+	 * @return		{@code true} if the file is image and {@code false} if it is not image
+	 */
+	private static boolean isImage(File file) {
+		
+		String name = file.getName().toLowerCase();
+		int dotIndex = name.lastIndexOf(".");
+		
+		if (dotIndex == -1) {
+			return false;
+		}
+		
+		String extension = name.substring(dotIndex + 1);
+		return IMAGE_EXTENSIONS.contains(extension);
+	}
+	
+	/**
+	 * Find image in {@code files} and create {@code Artwork} from this image.
+	 *
+	 * @param files			files to search for {@code Artwork} in
+	 * @return				{@code Artwork} from image in {@code files}
+	 * @throws IOException	if there is no image in {@code files}
+	 */
+	public static Artwork findAndCreateArtworkFromFiles(File[] files) throws IOException {
+		
+		for (File file : files) {
+			if (file.isFile()) {
+				try {
+					if (isImage(file)) {
+						return ArtworkFactory.createArtworkFromFile(file);
+					}
+				} catch (IOException e) {
+					continue;
+				}
+			}
+		}
+		
+		throw new IOException("No images in files");
+	}
+}
